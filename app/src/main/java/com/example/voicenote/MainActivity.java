6091e411d998e2f508b227fdecbbe8a2147b3f3a
root <root@kali.azatezalo>
2022-07-26 18:39:05 -0400

    Basic Vars created in main function.

--- app/src/main/java/com/example/voicenote/MainActivity.java
+++ app/src/main/java/com/example/voicenote/MainActivity.java
@@ -2,9 +2,29 @@ package com.example.voicenote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_AUDIO_PERMISSION_CODE=101;
+    MediaRecorder mediaRecorder;
+    MediaPlayer mediaPlayer;
+    ImageView ibRecord;
+    ImageView ibPlay;
+    TextView tvTime;
+    TextView tvRecordingPath;
+    ImageView ivSimpleBg;
+    boolean isRecording=true;
+    boolean isPlaying=false;
+
+    int seconds=0;
+    String path = null;
+    int dummySeconds=0;
+    int playableSeconds=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
